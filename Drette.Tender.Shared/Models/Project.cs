d2248d0e5973ab62a5fe897a3b930949afc86356
Jonathan Blais-Bélanger <joe_belanger@hotmail.com>
2018-11-06 11:18:12 -0500

    Added Project and Task's models

---
+++ Drette.Tender.Shared/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drette.Tender.Shared.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Drette.Tender.Shared/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drette.Tender.Shared.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int TaskInformationsId { get; set; }
+        public TaskInformations TaskInformations { get; set; }
+    }
+}
